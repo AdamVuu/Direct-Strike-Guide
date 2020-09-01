6ed61455722e2d7678333236b59182efe12a3bef
Adam Vu <adammvu2@gmail.com>
2020-08-31 23:15:33 -0400

    Adding in first choice in-game functionality for Terran

--- directstrike.py
+++ directstrike.py
@@ -24,13 +24,39 @@ while player == False:
     if player == "Terran":
         print("Your race is ", player,  ".")  
         print("You will be playing against a ", computer,  ".")
+        firstchoice = False
+        while firstchoice == False:
+        #set firstchoice to True
+            print('what will you open the game with?')
+            firstchoice = input('Units', 'Upgrades','Economy? ')
+            if firstchoice == 'Units':
+                print('What units would you like to open with?')
+                tUnits = input('Marine', 'Marauder', 'Reaper? ')
+                print('You have decided to open up with a ', tUnits, ".")
+            elif firstchoice == 'Upgrades':
+                print('What upgrades would you like to open with? ')
+                tUpgrades == input('Special Upgrades', 'Weapon', 'Armor')
+                print('You have decided to invest in ', tUpgrades, '.')
+            elif firstchoice == 'Economy':
+                print('You have decided to invest in economy and purchased a gas structure.')
+            else:
+                print("That's not a valid option. Choose another option.")
+        #firstchoice was set to True but we want it to be False so the loop continues
+        firstchoice == False 
+
 
     elif player == "Protoss":
         print("Your race is ", player,  ".")  
         print("You will be playing against a ", computer, ".")
+        firstchoice = False
+        player = input('Terran, Protoss, Zerg? ')
+        print('what will you open the game with?')
     elif player == "Zerg":
         print("Your race is ", player,  ".")  
         print("You will be playing against a ", computer, ".")
+        firstchoice = False
+        player = input('Terran, Protoss, Zerg? ')
+        print('what will you open the game with?')
     else:
         print("That's not a valid race. Pick a race.")
     #player was set to True, but we want it to be False so the loop continues

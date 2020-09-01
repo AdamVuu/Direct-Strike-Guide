07c9f18755e282566741fc3383d43e096a5134d5
Adam Vu <adammvu2@gmail.com>
2020-08-31 22:27:18 -0400

    added different race matchup and detection capability

--- directstrike.py
+++ directstrike.py
@@ -1,4 +1,34 @@
-# Interactive Direct Strike Guide for Starcraft Arcade Game Direct Strike
+# Interactive Guide for Starcraft Arcade Game Direct Strike
+
+from random import randint
+
+#create a list of race options
+race = ['Terran', 'Protoss', 'Zerg']
+
+#assign a random race to the computer
+computer = race[randint(0,2)]
+
+
+#asks for player name
 print('What is your name?')
 myName = input()
-print('Hi {}, what race will you be playing as?' .format(myName))
+
+#asks for player race
+print('Hi {}, what race will you be playing as? ' .format(myName))
+
+#set player to False
+player = False
+while player == False:
+#set player to True
+    player = input('Terran, Protoss, Zerg? ')
+    if player == "Terran":
+        print("Your race is ", player,  ".")  
+        print("You will be playing against a ", computer,  ".")
+    elif player == "Protoss":
+        print("Your race is ", player,  ".")  
+        print("You will be playing against a ", computer, ".")
+    elif player == "Zerg":
+        print("Your race is ", player,  ".")  
+        print("You will be playing against a ", computer, ".")
+    else:
+        print("That's not a valid race. Pick a race.")

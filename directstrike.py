958ccedee776dbe88aed2155cc56240f5d374fce
Adam Vu <adammvu2@gmail.com>
2020-09-09 22:26:14 -0400

    Creating lists to pull options for code

--- directstrike.py
+++ directstrike.py
@@ -8,29 +8,29 @@ race = ['Terran', 'Protoss', 'Zerg']
 #assign a random race to the computer
 computer = race[randint(0,2)]
 
-t1Units = ['Marine', 'Marauder', 'Reaper']
-t2Units = ['Ghost', 'Hellion', 'Hellbat', 'Siege Tank', 'Viking', 'Medivac', 'Banshee', 'Raven', 'Liberator' 'Widow Mine', 'Cyclone', ]
-t3Units = ['Thor', 'Battlecruiser']
+t1Units_list = ['Marine', 'Marauder', 'Reaper']
+t2Units_list = ['Ghost', 'Hellion', 'Hellbat', 'Siege Tank', 'Viking', 'Medivac', 'Banshee', 'Raven', 'Liberator' 'Widow Mine', 'Cyclone', ]
+t3Units_list = ['Thor', 'Battlecruiser']
 
-p1Units = ['Zealot', 'Adept', 'Stalker', 'Sentry']
-p2Units = ['Dark Templar', 'High Templar', 'Archon', 'Warp Prism', 'Observer', 'Immortal', 'Disruptor','Phoenix', 'Oracle', 'Void Ray']
-p3Units = ['Colossus', 'Carrier', 'Tempest']
+p1Units_list = ['Zealot', 'Adept', 'Stalker', 'Sentry']
+p2Units_list = ['Dark Templar', 'High Templar', 'Archon', 'Warp Prism', 'Observer', 'Immortal', 'Disruptor','Phoenix', 'Oracle', 'Void Ray']
+p3Units_list = ['Colossus', 'Carrier', 'Tempest']
 
-z1Units = ['Zergling', 'Baneling', 'Roach', 'Ravager', 'Queen']
-z2Units = ['Hydralisk', 'Lurker', 'Mutalisk', 'Corruptor', 'Infestor', 'Overseer']
-z3Units = ['Viper', 'Utralisk', 'Brood Lord']
+z1Units_list = ['Zergling', 'Baneling', 'Roach', 'Ravager', 'Queen']
+z2Units_list = ['Hydralisk', 'Lurker', 'Mutalisk', 'Corruptor', 'Infestor', 'Overseer']
+z3Units_list = ['Viper', 'Utralisk', 'Brood Lord']
 
-t1Upgrades = ['Infantry Weapons 1', 'Infantry Armor 2', 'Stimpack', 'Concussive Shells']
-t2Upgrades = ['Infantry Weapons 2', 'Infantry Armor 2', 'Vehicle Weapons 1', 'Vehicle Weapons 2', 'Ship Weapons 1', 'Ship Weapons 2', 'Vehicle/Ship Armor 1,' 'Vehicle/Ship Armor 2', 'Infernal Pre-lighter,' 'Raven Energy', 'Medevac Boosters']
-#t3Upgrades =
+t1Upgrades_list = ['Infantry Weapons 1', 'Infantry Armor 2', 'Stimpack', 'Concussive Shells']
+t2Upgrades_list = ['Infantry Weapons 2', 'Infantry Armor 2', 'Vehicle Weapons 1', 'Vehicle Weapons 2', 'Ship Weapons 1', 'Ship Weapons 2', 'Vehicle/Ship Armor 1,' 'Vehicle/Ship Armor 2', 'Infernal Pre-lighter,' 'Raven Energy', 'Medevac Boosters']
+t3Upgrades_list = ['Infantry Weapons 3', 'Infantry Armor 3', 'Vehicle Weapons 3', 'Ship Weapons 3', 'Vehicle/Ship Armor 3']
 
-p1Upgrades = ['Ground Weapons 1', 'Ground Armor 1', 'Shields 1', ]
-#p2Upgrades =
-#p3Upgrades =
+p1Upgrades_list = ['Ground Weapons 1', 'Ground Armor 1', 'Shields 1', ]
+p2Upgrades_list = ['Ground Weapons 2', 'Ground Armor 2', 'Shields 2', 'Air Weapons 1', 'Air Armor 1', 'Air Weapons 2', 'Air Armor 2', 'Charge', 'Resonating Glaives', 'Blink', 'Shadow Step', 'Psionic Storm']
+p3Upgrades_list = ['Ground Weapons 3', 'Ground Armor 3', 'Shields 3', 'Air Weapons 3', 'Air Armor 3']
 
-z1Upgrades = ['Melee Attacks 1', 'Missile Attacks 1', 'Ground Carapace 1', 'Metabolic Boost', 'Pneumatized Carapace']
-#z2Upgrades = 
-#z3Upgrades = 
+z1Upgrades_list = ['Melee Attacks 1', 'Missile Attacks 1', 'Ground Carapace 1', 'Metabolic Boost', 'Pneumatized Carapace']
+z2Upgrades_list =['Melee Attacks 2', 'Missile Attacks 2', 'Ground Carapace 2', 'Flyer Attacks 1', 'Flyer Attacks 2', 'Flyer Carapace 1', 'Flyer Carapace 2']
+z3Upgrades_list = ['Melee Attacks 3', 'Missile Attacks 3', 'Ground Carapace 3', 'Flyer Attacks 1', 'Flyer Carapace 3']
 
 #asks for player name
 print('What is your name?')

bf7fe1904fd4d8fad99a909275f819ef41a0aa38
AdamVuu <adammvu2@gmail.com>
2021-02-08 16:00:16 -0500

    creating UI

--- zerg.py
+++ zerg.py
@@ -3,5 +3,5 @@ z2Units = ['Hydralisk', 'Lurker', 'Mutalisk', 'Corruptor', 'Infestor', 'Overseer
 z3Units = ['Viper', 'Utralisk', 'Brood Lord']
 
 z1Upgrades = ['Melee Attacks 1', 'Missile Attacks 1', 'Ground Carapace 1', 'Metabolic Boost', 'Pneumatized Carapace']
-z2Upgrades = 
-z3Upgrades = 
+z2Upgrades = ['Swag', 'Swag']
+z3Upgrades = []
